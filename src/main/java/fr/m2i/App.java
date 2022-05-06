f35022a98461a4d591a2cf2594c49d79857b9eaa
Tiity <anthony.cadet@ocito.com>
2022-05-06 13:12:06 +0200

    Create java project

---
+++ src/main/java/fr/m2i/App.java
@@ -0,0 +1,13 @@
+package fr.m2i;
+
+public final class App {
+    private App() {
+    }
+
+    /**
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
